2f7c965d63b7234905da2cf07c976f5da28e1387
sapol pornphaisalsak <noby2500@gmail.com>
2019-02-17 21:10:06 +0700

    test button reaect native #1

--- TestAPpReactProject/App.js
+++ TestAPpReactProject/App.js
@@ -8,7 +8,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -22,9 +22,13 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native! Test1</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <Button
+  title="Solid Button"/>
+
+  <Button
+  title="Clear button"
+  type="clear"
+/>
       </View>
     );
   }
